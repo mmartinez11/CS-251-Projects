d0a0baa7fd88ae49e2fe31852b1d59644aee76f9
Manuel Martinez <33674827+alaskanbear48@users.noreply.github.com>
2020-06-10 14:24:44 -0500

    Update bst.h

--- Project 02/bst.h
+++ Project 02/bst.h
@@ -1,18 +1,24 @@
+//This file contains the code for my Binary Search Tree. This BST will serve as a tool to store important information such as movie name, ID, and the amount of reviews it recieved in each category.
+//This tree is able to store a key and also a struct that has stored all the information that we need about each individual movie. In addition the tree has private and public functions
+//that allow it to share information with the user upon being called. Information such as the height, size, and the information stored in each individual struct. Although not all functions
+//will be used in this specific project it is still helpfull to have them in the scenario that the program is modified for other purposes. In addition they are useful for debugging.  
+
 #pragma once
 
 #include <iostream>
-#include <algorithm>  // std::max
+#include <algorithm>
 
-using namespace std; 
-
+using namespace std;
+ 
+//Template is updated to include Key and Value 
 template<typename TKey, typename TValue>
-class binarysearchtree
-{
+class binarysearchtree{
+
 private:
-  struct NODE
-  {
+  struct NODE{
+
     TKey  Key;
-	TValue Value;
+    TValue Value;
     NODE* Left;
     NODE* Right;
   };
@@ -21,7 +27,7 @@ private:
   NODE* Root;
   int   Size; 
   
-  
+  //Recursive private function that traverses the BST and returns the height of the tree 
   int _height(NODE* cur)
   {
     if (cur == nullptr)
@@ -34,17 +40,21 @@ private:
       return 1 + std::max(heightL, heightR);
     }
   }
-  
-  void _copy(NODE* cur)
-  {
-	  if (cur == nullptr)
-	  {
+
+//Recursive private copy constructor. Its job is to traverse the orginal BST and store its 
+//Keys and Values into the copy BST.    
+void _copy(NODE* cur){
+
+    //Checks for null current 
+    if (cur == nullptr){
       return;
 	  }
-    else
-    {
-	 
-	 insert(cur->Key, cur->Value);
+
+    else{
+	
+      //Inserts the root values of the original BST into the 
+      //root values of the copy BST first. Then it begins to traverse 
+      insert(cur->Key, cur->Value);
       _copy(cur->Left);
       _copy(cur->Right);
     }
@@ -52,8 +62,10 @@ private:
   
   }
   
-  void _inorder(NODE* cur)
-  {
+  //Recursive function used to return the key values of the BST in ascending order.
+  //Traverses the BST left to right.
+  void _inorder(NODE* cur){
+
     if (cur == nullptr)
       return;
     else
@@ -64,142 +76,146 @@ private:
     }
   }
   
-  void _destroy(NODE* current)
-  {
-	  if(current)
-	  {
+  //Recursive private destructor. When it is called it deletes all the nodes in 
+  //the BST.
+  void _destroy(NODE* current){
+
+	  if(current){
+
 	  _destroy(current->Left);
 	  _destroy(current->Right);
 	  delete current;
 	  }
-  
   }
   
   public:
-  
-  binarysearchtree()
-  {
+   
+  //Default constructor. Creates a BST by making a Root pointer.
+  binarysearchtree(){
+
     Root = nullptr;
     Size = 0;
   }
   
-  //Copy constructor:
-  binarysearchtree(const binarysearchtree& other)
-  {
+  //Copy constructor: Calls Private Recursive Private constructor
+  //Creates a Root pointer for the copy BST
+  binarysearchtree(const binarysearchtree& other){
+
 	  Root = nullptr;
 	  Size = 0;
 	  
 	  _copy(other.Root);
-  
-  
   }
-  
-  //Destructor 
-  virtual ~binarysearchtree()
-  {
+
+  //Destructor: Calls a private destructor recursive function.
+  //Passes a pointer pointing to the Root of the BST 
+  virtual ~binarysearchtree(){
+
       NODE* current = Root;
 	 _destroy(current);
     
   }
   
-  int size()
-  {
+  //Function that returns the Size of the BST
+  int size(){
+
     return Size;
   }
   
-  int height()
-  {
+  //Function that returns the height of the BST by calling a 
+  //Private recursive function.
+  int height(){
+
     return _height(Root);
   }
   
-  
-  TValue* search(TKey key)
-  {
+  //Function is used to search the BST for a specific Key. The function 
+  //traverses the BST checking every node for the Key and returns the respective value.   
+  TValue* search(TKey key){
+
     NODE* cur = Root;
 
-    while (cur != nullptr)
-    {
-      if (key == cur->Key)  // already in tree
-        return &(cur->Value);
+    while (cur != nullptr){
 
-      if (key < cur->Key)  // search left:
-      {
+      if (key == cur->Key){ // Already in the tree
+        
+	return &(cur->Value);
+      }
+      if (key < cur->Key){  //Search to the left
+      
         cur = cur->Left;
       }
-      else
-      {
+      else{
+
         cur = cur->Right;
       }
-    }//while  
+    } 
 
-    // if get here, not found
+    //Not found
     return nullptr;
   }
   
-  void insert(TKey key, TValue value)
-  {
+  //Function is used to insert the Key and Value into a new node in the BST
+  void insert(TKey key, TValue value){
+
     NODE* prev = nullptr;
     NODE* cur = Root;
 
-    //
-    // 1. Search to see if tree already contains key:
-    //
-    while (cur != nullptr)
-    {
-      if (key == cur->Key)  // already in tree
-        return;
+   
+    //Searches to see if tree already contains a key
+    while (cur != nullptr){
 
-      if (key < cur->Key)  // search left:
-      {
+      if (key == cur->Key){ //Found in tree
+        return;
+	}
+
+      if (key < cur->Key){ //Searches left
+     
         prev = cur;
         cur = cur->Left;
       }
-      else
-      {
+      else{
+
         prev = cur;
         cur = cur->Right;
       }
-    }//while
+    }
 
-    //
-    // 2. if we get here, key is not in tree, so allocate
-    // a new node to insert:
-    // 
+    //Key is not in tree, so it allocates
+    //a brand new node to insert with the Key and Value
     NODE* newNode;
     newNode = new NODE();
+    
     newNode->Key = key;
-	newNode->Value = value;
+    newNode->Value = value;
+    
     newNode->Left = nullptr;
     newNode->Right = nullptr;
 
-    //
-    // 3. link in the new node:
-    //
-    // NOTE: cur is null, and prev denotes node where
-    // we fell out of the tree.  if prev is null, then
-    // the tree is empty and the Root pointer needs 
-    // to be updated.
-    //
-    if (prev == nullptr)
-	{
-      Root = newNode;
+    //Links the new node using cur as null
+    //prev is used to show were we fell out of the tree.  
+    //if prev is null, then the tree is empty and the Root pointer needs to be updated.
+    if (prev == nullptr){
+
+      	Root = newNode;
 	}
-    else if (key < prev->Key)
-	{
-      prev->Left = newNode;
+    else if (key < prev->Key){
+
+      	prev->Left = newNode;
 	}
-    else
-	{
-      prev->Right = newNode;
+    else{
+
+      	prev->Right = newNode;
 	}
-    // 
-    // 4. update size and we're done:
-    //
+  
+    //Updates size
     Size++;
   }
   
-  void inorder()
-  {
+  //Function that returns the key values of the BST
+  //Calls a private recursive function 
+  void inorder(){
+
     cout << "Inorder: ";
 
     _inorder(Root);
@@ -207,13 +223,14 @@ private:
     cout << endl;
   }
   
-   void clear()
-  {
+   //Function that clears the BST. 
+   //Calls a private recursive destructor
+   void clear(){
       
-	  NODE* cure = Root;
+    NODE* cure = Root;
      _destroy(cure);
-    // Re-initialize root pointer and size to "empty":
-    //
+
+    // Re-initializes root pointer and size to "empty"
     Root = nullptr;
     Size = 0;
     
